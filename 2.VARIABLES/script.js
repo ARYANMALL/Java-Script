0a69d0d0a4480cff78b3cb670c0b524f71ac2c95
ARYANMALL <ay2865502@gmail.com>
2026-01-31 19:08:17 +0530

    Add script.js file with variable declarations and initializations

---
+++ 2.VARIABLES/script.js
@@ -0,0 +1,25 @@
+//keyword(let,var,const)
+//variable name(container)
+//value
+
+//Variable declaration and initialization
+let  age = 30;
+console.log(age);
+
+//Declaration first,then assignment
+let country;
+country="Ghana";
+console.log(country);
+
+
+//Multiple declarations,then assignments:
+let firstName, lastName;
+firstName="John";
+lastName="Doe";
+console.log(firstName ,lastName);
+console.log(firstName);
+console.log(lastName);
+
+//Multiple declarations in one line:
+let age1=30, job='Developer', country1="India";
+console.log(age1,job,country1);
