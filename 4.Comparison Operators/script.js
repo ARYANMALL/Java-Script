c3fd6ce2083277997f29f5131764c7b071177ffc
ARYANMALL <ay2865502@gmail.com>
2026-02-19 15:14:21 +0530

    Add HTML and JavaScript files for comparison operators demonstration

---
+++ 4.Comparison Operators/script.js
@@ -0,0 +1,35 @@
+let a = 20;
+let b = 30;
+
+//! ==
+let isEqual = a == b; // false
+console.log(isEqual);
+
+//! !=
+let isNotEqual = a != b; // true
+console.log(isNotEqual);
+
+//! ===
+let isStrictEqual = a === b; // false
+console.log(isStrictEqual); 
+
+//! !==
+let isStrictNotEqual = a !== b; // true
+console.log(isStrictNotEqual);
+
+//! >
+let isGreaterThan = a > b; // false
+console.log(isGreaterThan);
+
+//! <
+let isLessThan = a < b; // true
+console.log(isLessThan);
+
+//! >=
+let isGreaterThanOrEqual = a >= b; // false
+console.log(isGreaterThanOrEqual);
+
+//! <=
+let isLessThanOrEqual = a <= b; // true
+console.log(isLessThanOrEqual);
+
